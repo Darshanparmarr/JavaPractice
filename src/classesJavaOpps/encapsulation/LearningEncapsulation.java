8081440855eb10464752824f0a241eed71910b30
Darshanparmarr <darshuparmar67@gmail.com>
2026-07-02 13:00:28 +0530

    Learning encapsulation and toString method

--- src/classesJavaOpps/encapsulation/LearningEncapsulation.java
+++ src/classesJavaOpps/encapsulation/LearningEncapsulation.java
@@ -3,6 +3,7 @@ package classesJavaOpps.encapsulation;
 class Employee{
     private  int empId;
     private String empName;
+    private Address address;
 
     public int getEmpId(){
         return empId;
@@ -20,6 +21,46 @@ class Employee{
         this.empName = empName;
     }
 
+    public Address getAddress(){
+        return address;
+    }
+
+    public void setAddress(Address address){
+        this.address = address;
+    }
+
+    @Override
+    public String toString(){
+        return "Employee Id : " + empId + " , Employee Name : " + empName + " , Employee Adress : " + address;
+    }
+
+}
+
+class  Address{
+    private String apartmentName;
+    private String city;
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getApartmentName() {
+        return apartmentName;
+    }
+
+    public void setApartmentName(String apartmentName) {
+        this.apartmentName = apartmentName;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "apartmentName= " + apartmentName +
+                ", city= " + city;
+    }
 }
 
 public class LearningEncapsulation {
@@ -27,6 +68,13 @@ public class LearningEncapsulation {
         Employee employee = new Employee();
         employee.setEmpName("Ram");
         employee.setEmpId(21);
-        System.out.println("Id = " + employee.getEmpId() + " , Name = "+ employee.getEmpName());
+
+        Address address = new Address();
+        address.setCity("Tokyo");
+        address.setApartmentName("Oberoi Skycity");
+
+        employee.setAddress(address);
+        System.out.println(employee);
+        System.out.println(address);
     }
 }
