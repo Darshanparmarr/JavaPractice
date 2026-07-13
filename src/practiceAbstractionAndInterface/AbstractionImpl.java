7be7c06d6350c86dd8773bd2882f2549ad5c19f5
Darshanparmarr <darshuparmar67@gmail.com>
2026-07-13 12:58:43 +0530

    Interface Implementation

--- src/practiceAbstractionAndInterface/AbstractionImpl.java
+++ src/practiceAbstractionAndInterface/AbstractionImpl.java
@@ -1,43 +1,71 @@
 package practiceAbstractionAndInterface;
 
-abstract class AbstractCar {
-    String carName;
-    String carColor;
-    int mileage;
+//abstract class AbstractCar {
+//    String carName;
+//    String carColor;
+//    int mileage;
+//
+//    public AbstractCar(String carName, String colorName, int mileage) {
+//        this.carName = carName;
+//        this.carColor = colorName;
+//        this.mileage = mileage;
+//    }
+//
+//    public void showCarDetails(){
+//        System.out.println(this.carName);
+//        System.out.println(this.carColor);
+//        System.out.println(this.mileage);
+//    }
+//
+//    public abstract void modelName();
+//
+//}
+//
+//class Ferrari extends AbstractCar{
+//
+//    public Ferrari(String carName, String carColor, int mileage){
+//        super(carName, carColor, mileage);
+//    }
+//
+//    public void modelName() {
+//        System.out.println("12Cilindri");
+//    }
+//
+//
+//}
+//
+//public class AbstractionImpl {
+//    public static void main(String[] args) {
+//        Ferrari ferrari = new Ferrari("Ferrari", "Red", 24);
+//        ferrari.showCarDetails();
+//        ferrari.modelName();
+//    }
+//}
 
-    public AbstractCar(String carName, String colorName, int mileage) {
-        this.carName = carName;
-        this.carColor = colorName;
-        this.mileage = mileage;
+
+interface Carable{
+    String carName = "Ferrari";
+    String colorName = "Red";
+    int mileage = 23;
+    void modelName();
+    void showCarDetails();
+}
+
+class Ferrari implements Carable{
+
+    public void modelName(){
+        System.out.println("12 Cilindri");
     }
 
     public void showCarDetails(){
-        System.out.println(this.carName);
-        System.out.println(this.carColor);
-        System.out.println(this.mileage);
+        System.out.println(Carable.carName);
+        System.out.println(Carable.colorName);
+        System.out.println(Carable.mileage);
     }
 
-    public abstract void modelName();
-
-}
-
-class Ferrari extends AbstractCar{
-
-    public Ferrari(String carName, String carColor, int mileage){
-        super(carName, carColor, mileage);
-    }
-
-    public void modelName() {
-        System.out.println("12Cilindri");
-    }
-
-
-}
-
-public class AbstractionImpl {
     public static void main(String[] args) {
-        Ferrari ferrari = new Ferrari("Ferrari", "Red", 24);
-        ferrari.showCarDetails();
-        ferrari.modelName();
+        new Ferrari().modelName();
+        new Ferrari().showCarDetails();
     }
+
 }
